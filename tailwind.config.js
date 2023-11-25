37d8c818f084b2ddf79053ce33927531ecc7686a
JuanIgnaso <104755375+JuanIgnaso@users.noreply.github.com>
2023-11-25 17:32:16 +0100

    Añadido darkmode
    Añadido darkmode a la web.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        //Tonos de azul por número, menor-> mas claro, mayor->mas oscuro
+        'portfolioAzul1' : '#e7eaf6',
+        'portfolioAzul2': '#a2a8d3', 
+        'porfolioAzul3' : '#38598b',
+        'portfolioAzul4' : '#113f67'
+
+        //Otros colores
+      }
+    },
     
   },
   plugins: [],
